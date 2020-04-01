25c28cb6324900beee9c4617b5e36ddfedd647bf
Filonitta <filonitta@gmail.com>
2020-04-01 23:20:46 +0300

    feat: style Today statistics

--- src/components/Today/Statistics.jsx
+++ src/components/Today/Statistics.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 import NoData from '@shared/NoData';
 
@@ -11,21 +12,25 @@ const Statistics = (props) => {
 	if (!Object.keys(info)) return <NoData />
 
 	return (
-		<div className="card card-body bg-light">
-			<h2 className="mt-4 mb-4">
-				{/* <img src={info.countryInfo.flag} style={{maxWidth: '75px', marginRight: '5px'}} alt=""/> */}
-				{info.country}
-				<img src={info.countryInfo.flag} style={{ position: 'absolute', right: '1.25rem' }} alt=""/>
-			</h2>
-			<dl>
-				<dt>Total Cases</dt><dd><span className="badge badge-secondary">{info.cases}</span></dd>
-				<dt>Today cases</dt><dd><span className="badge badge-secondary">{info.todayCases}</span></dd>
-				<dt>Total Deaths</dt><dd><span className="badge badge-secondary">{info.todayDeaths}</span></dd>
-				<dt>Today Deaths</dt><dd><span className="badge badge-secondary">{info.todayDeaths}</span></dd>
-				<dt>Recovered</dt><dd><span className="badge badge-secondary">{info.recovered}</span></dd>
-				<dt>Active</dt><dd><span className="badge badge-secondary">{info.active}</span></dd>
-				<dt>Critical</dt><dd><span className="badge badge-secondary">{info.critical}</span></dd>
-			</dl>
+		<div className="card bg-light">
+			<div className="card-header">
+				<h4 className="mb-0">{info.country}</h4>
+			</div>
+			<div className="card-body">
+					<img src={info.countryInfo.flag} style={{ position: 'absolute', right: '1.25rem' }} alt=""/>
+				<dl>
+					<dt>Total Cases</dt><dd><span className="badge badge-secondary">{info.cases}</span></dd>
+					<dt>Today cases</dt><dd><span className="badge badge-secondary">{info.todayCases}</span></dd>
+					<dt>Total Deaths</dt><dd><span className="badge badge-secondary">{info.todayDeaths}</span></dd>
+					<dt>Today Deaths</dt><dd><span className="badge badge-secondary">{info.todayDeaths}</span></dd>
+					<dt>Recovered</dt><dd><span className="badge badge-secondary">{info.recovered}</span></dd>
+					<dt>Active</dt><dd><span className="badge badge-secondary">{info.active}</span></dd>
+					<dt>Critical</dt><dd><span className="badge badge-secondary">{info.critical}</span></dd>
+				</dl>
+			</div>
+			<div className="card-footer">
+				<span className="badge">Date updated: {moment(info.updated).format('MM-DD-YYYY')}</span>
+			</div>
 		</div>
 	);
 };
