37369fc0e08fa6e857a2b0e04731d7960bfc38d6
Filonitta <filonitta@gmail.com>
2020-04-06 00:31:41 +0300

    feat: make Search dependent on Sorting

--- src/components/shared/SearchField.jsx
+++ src/components/shared/SearchField.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
@@ -6,12 +6,31 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 const SearchField = (props) => {
 	const {
 		list,
-		onSearch
+		onSearch,
+		value,
 	} = props;
+
+	const [currentValue, setCurrentValue] = useState(value);
+	const [originalList, setOriginalList] = useState([]);
+	
+	useEffect(() => {
+		!originalList.length && setOriginalList(list);
+		// if (originalList.length) {
+		// 	console.log('list', list);
+		// 	console.log('originalList', originalList);
+		// }
+
+		// console.log('currentValue', currentValue)
+		// currentValue && onSearch(originalList.filter(item => item.country.toLowerCase().startsWith(currentValue)), currentValue);
+	}, [originalList, list, currentValue]);
+	
+	/* useEffect(() => {
+	}, [currentValue, originalList]); */
 
 	const filter = event => {
 		const value = event.target.value.toLowerCase();
-		onSearch(list.filter(item => item.country.toLowerCase().startsWith(value)) )
+		setCurrentValue(value);
+		onSearch(originalList.filter(item => item.country.toLowerCase().startsWith(value)), value);
 	}
 
 	return (
@@ -34,7 +53,8 @@ const SearchField = (props) => {
 
 SearchField.propTypes = {
 	list: PropTypes.array,
-	onSearch: PropTypes.func
+	onSearch: PropTypes.func,
+	value: PropTypes.string,
 };
 
 export default SearchField;
