d3a1296768d775b6ed2577234cbbf8d77e92b48b
Filonitta <filonitta@gmail.com>
2020-04-13 16:32:21 +0300

    feat: aggregate provinces by country on all countries tab

--- src/components/HistoricalCountries/index.js
+++ src/components/HistoricalCountries/index.js
@@ -22,29 +22,71 @@ const HistoricalCountries = () => {
 		}
 	} = store;
 
-	// const [countries, setCountries] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 
 	useEffect(() => {
 		async function fetchCountries() {
 			setIsLoading(true);
-			const data = await api.getCountries(360);
+			let data = await api.getCountries(360);
 			
-			data.sort((a, b) => b.country > a.country ? -1 : b.country < a.country ? 1 : 0).forEach(item => {
-				item.country = item.country.capitalize();
-				if (item.province) {
-					item.province = item.province.capitalize();
-				}
-			});
+			data = aggregateByCountryName(data);
 			
-			// setCountries(data);
-			// !arraysEqual(data, countries) &&
 			dispatch( allListAction(data) );
 			setIsLoading(false);
 		}
 
 		fetchCountries();
 	}, [dispatch]);
+
+	const aggregateByCountryName = (list) => {
+		const countries = [];
+		
+		list.forEach((item, i, source) => {
+			const country = item.country;
+			
+			if (countries.find(item => item.country === country)) return;
+
+			const timeline = {
+				cases: {},
+				deaths: {},
+				recovered: {}
+			};
+	
+			const provinces = source.filter(item => item.country === country);
+
+			const result = {
+				country,
+				timeline
+			};
+
+			provinces.forEach((province) => {
+				const dates = Object.keys(province.timeline.cases);
+				
+				const values = Object.keys(timeline).reduce((values, field) => {
+					values[field] = Object.values(province.timeline[field]);
+					return values;
+				}, {});
+				
+				dates.forEach((date, i) => {
+					Object.keys(timeline).forEach(field => {
+						result.timeline[field][date] = result.timeline[field][date] ? result.timeline[field][date] + values[field][i] : values[field][i];
+					});
+				});
+			}, item);
+
+			countries.push(result);
+
+			/* { // test
+				const testField = 'recovered';
+				const dates = Object.keys(provinces[0].timeline.cases);
+				const lastDate = dates[dates.length - 1];
+				const value = provinces.reduce((value, item) => value + item.timeline[testField][lastDate], 0);
+				console.log(value === result.timeline[testField][lastDate]);
+			} */
+		});
+
+		return countries;
+	};
 
 	return (
 		<>
