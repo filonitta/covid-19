b8c7ec49df21d5195c7009d0581891dfc9273fca
Filonitta <filonitta@gmail.com>
2020-04-13 17:46:32 +0300

    feat: apply aggregateByCountryName from module

--- src/components/HistoricalCountries/index.js
+++ src/components/HistoricalCountries/index.js
@@ -7,7 +7,7 @@ import api from '@/services/api.class';
 import Statistics from './Statistics';
 import Context from '@redux/store';
 import { allListAction } from '@redux/actions';
-import { arraysEqual } from '@utils/array';
+import { aggregateByCountryName } from '@utils/countries';
 
 String.prototype.capitalize = function () {
 	const value = this.valueOf();
@@ -37,56 +37,6 @@ const HistoricalCountries = () => {
 
 		fetchCountries();
 	}, [dispatch]);
-
-	const aggregateByCountryName = (list) => {
-		const countries = [];
-		
-		list.forEach((item, i, source) => {
-			const country = item.country;
-			
-			if (countries.find(item => item.country === country)) return;
-
-			const timeline = {
-				cases: {},
-				deaths: {},
-				recovered: {}
-			};
-	
-			const provinces = source.filter(item => item.country === country);
-
-			const result = {
-				country,
-				timeline
-			};
-
-			provinces.forEach((province) => {
-				const dates = Object.keys(province.timeline.cases);
-				
-				const values = Object.keys(timeline).reduce((values, field) => {
-					values[field] = Object.values(province.timeline[field]);
-					return values;
-				}, {});
-				
-				dates.forEach((date, i) => {
-					Object.keys(timeline).forEach(field => {
-						result.timeline[field][date] = result.timeline[field][date] ? result.timeline[field][date] + values[field][i] : values[field][i];
-					});
-				});
-			}, item);
-
-			countries.push(result);
-
-			/* { // test
-				const testField = 'recovered';
-				const dates = Object.keys(provinces[0].timeline.cases);
-				const lastDate = dates[dates.length - 1];
-				const value = provinces.reduce((value, item) => value + item.timeline[testField][lastDate], 0);
-				console.log(value === result.timeline[testField][lastDate]);
-			} */
-		});
-
-		return countries;
-	};
 
 	return (
 		<>
