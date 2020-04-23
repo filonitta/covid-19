c6a5cdff30211d73cd4bee7344bce7b4b04149ed
Filonitta <filonitta@gmail.com>
2020-04-23 21:32:53 +0300

    feat: implement api hook on countries tab

--- src/components/HistoricalCountries/index.js
+++ src/components/HistoricalCountries/index.js
@@ -1,49 +1,39 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import Spinner from 'react-bootstrap/Spinner';
-
 import 'react-datepicker/dist/react-datepicker.css';
 
-import api from '@/services/api';
 import Statistics from './Statistics';
 import Context from '@redux/store';
 import { allListAction } from '@redux/actions';
 import { aggregateByCountryName } from '@utils/countries';
 import ErorMessage from '@shared/ErorMessage';
+import useApi from '@/services/api';
 
 String.prototype.capitalize = function () {
 	const value = this.valueOf();
 	return `${value.substring(0, 1).toUpperCase()}${value.substring(1)}`;
-}
+};
 
 const HistoricalCountries = () => {
 	const { store, dispatch } = useContext(Context);
-	const [errorMessage, setErrorMessage] = useState('');
 
 	const {
-		all: {
-			list: countries,
-		}
+		all: { list: countries }
 	} = store;
 
-	const [isLoading, setIsLoading] = useState(false);
+	const {
+		data: info,
+		isError,
+		errorMessage,
+	} = useApi('historical?lastdays=0', []);
 
 	useEffect(() => {
-		async function fetchCountries() {
-			setIsLoading(true);
-			let data = await api.getCountries(0).catch(setErrorMessage);
-			
-			if (data) {
-				data = aggregateByCountryName(data);
-				dispatch( allListAction(data) );
-			}
-			
-			setIsLoading(false);
-		}
+		if (!info.length) return;
+		const data = aggregateByCountryName(info);
+		dispatch(allListAction(data));
+	}, [dispatch, info]);
 
-		fetchCountries();
-	}, [dispatch]);
-
-	if (errorMessage) return <ErorMessage />;
+	if (isError) return <ErorMessage message={errorMessage} />;
 
 	return (
 		<>
