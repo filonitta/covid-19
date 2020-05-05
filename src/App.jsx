07c033cf2c0b76c13f89949055253f9befd3cc31
Filonitta <filonitta@gmail.com>
2020-05-05 15:20:19 +0300

    feat: add online/offline detection

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom';
 import React, { useReducer } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Offline, Online } from 'react-detect-offline';
+
 import * as serviceWorker from './serviceWorker';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,6 +11,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
 import { routes } from '@/routes.js';
+import Container from '@/components/pages/Container';
 
 import {
 	initialState,
@@ -20,17 +23,28 @@ import Context from '@redux/store';
 const App = () => {
 	const [store, dispatch] = useReducer(reducer, initialState);
 
-	return (
-		<Context.Provider value={{ store, dispatch }}>
-			<Router basename="/">
-				<Switch>
-					{routes.map((route, i) => {
-						return <Route key={i} exact={route.exact} path={route.path ? route.path : null} component={route.component} />
-					})}
-				</Switch>
-			</Router>
-		</Context.Provider>
-	);
+	return <>
+		<Container>
+			<Online>
+				<Context.Provider value={{ store, dispatch }}>
+					<Router basename="/">
+						<Switch>
+							{routes.map((route, i) => {
+								return <Route key={i} exact={route.exact} path={route.path ? route.path : null} component={route.component} />
+							})}
+						</Switch>
+					</Router>
+				</Context.Provider>
+			</Online>
+			<Offline>
+				<div className="card bg-light">
+					<div className="card-body">
+						You are currently offline!
+					</div>
+				</div>
+			</Offline>
+		</Container>
+	</>;
 };
 
 ReactDOM.render(<App />, document.getElementById('app'));
