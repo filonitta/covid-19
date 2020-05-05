998ae39d5b004f7fad606a62c50b689d270e0590
Filonitta <filonitta@gmail.com>
2020-05-05 15:22:39 +0300

    v2.2.6

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,9 +2,6 @@ import React, { useState } from 'react';
 
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
-import Accordion from 'react-bootstrap/Accordion';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 import './Home.scss';
 import Total from '@base/src/components/Total';
@@ -14,53 +11,30 @@ import HistoricalCountries from '@base/src/components/HistoricalCountries';
 
 const Home = () => {
 	const [activeTab, setActiveTab] = useState('total');
-	const [isShown, setIsShown] = useState(false);
 
 	const onSelectTab = tab => {
 		setActiveTab(tab);
 	}
 
 	return (
-		<div className="container">
-			<div className="page-header mt-4 mb-3 mb-md-5">
-				<h1>COVID-19 statistics</h1>
-			</div>
+		<>
+		<Tabs defaultActiveKey={activeTab} onSelect={onSelectTab} variant="pills">
+			<Tab eventKey="total" title="Total" disabled={activeTab === 'total'} unmountOnExit>
+				<Total />
+			</Tab>
+			<Tab eventKey="today" title="Today" disabled={activeTab === 'today'} unmountOnExit>
+				<Today />
+			</Tab>
+			<Tab eventKey="all" title="Historical: all countries" disabled={activeTab === 'all'} unmountOnExit>
+				<HistoricalCountries />
+			</Tab>
+			<Tab eventKey="day" title="Historical: per day" disabled={activeTab === 'day'} unmountOnExit>
+				<HistoricalPerDay />
+			</Tab>
+		</Tabs>
 
-			<Tabs defaultActiveKey={activeTab} onSelect={onSelectTab} variant="pills">
-				<Tab eventKey="total" title="Total" disabled={activeTab === 'total'} unmountOnExit>
-					<Total />
-				</Tab>
-				<Tab eventKey="today" title="Today" disabled={activeTab === 'today'} unmountOnExit>
-					<Today />
-				</Tab>
-				<Tab eventKey="all" title="Historical: all countries" disabled={activeTab === 'all'} unmountOnExit>
-					<HistoricalCountries />
-				</Tab>
-				<Tab eventKey="day" title="Historical: per day" disabled={activeTab === 'day'} unmountOnExit>
-					<HistoricalPerDay />
-				</Tab>
-			</Tabs>
-
-			<footer className="bg-light p-2 text-right mt-5">
-				<Accordion>
-					<div>
-						<div>
-							<Accordion.Toggle as="div" variant="link" eventKey="0" className="button" onClick={() => setIsShown(!isShown)}>
-								<FontAwesomeIcon title={isShown ? 'Hide info' : 'Show info'} icon={faInfoCircle} />
-							</Accordion.Toggle>
-						</div>
-						<Accordion.Collapse eventKey="0">
-							<div>
-								<div><small>Used public API: <a href="https://corona.lmao.ninja" target="_blank" rel="noopener noreferrer">https://corona.lmao.ninja</a></small></div>
-								<div><small>Developed by: <a href="https://github.com/filonitta/covid-19" target="_blank" rel="noopener noreferrer">filonitta</a></small></div>
-							</div>
-						</Accordion.Collapse>
-					</div>
-				</Accordion>
-				
-				{/* <small>Used public API: <a href="https://corona.lmao.ninja" target="_blank" rel="noopener noreferrer">https://corona.lmao.ninja</a></small> */}
-			</footer>								
-		</div>
+										
+		</>
 	)
 }
 
--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class NotFound extends React.Component {
 	render() {
-		return <h2>Page not found</h2>;
+		return (
+			<div className="card bg-light">
+				<div className="card-header">
+					<h4 className="mb-0">Page not found</h4>
+				</div>
+				<div className="card-body">
+					Go back to <Link to="/">home page</Link>
+				</div>
+			</div>
+		);
 	}
 }
